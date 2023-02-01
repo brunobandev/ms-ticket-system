d48e809159d164bae57885c80ef6a80cbce316ea
Bruno Bandeira <brunoban.dev@gmail.com>
2023-02-01 19:55:41 +0000

    handling error

---
+++ auth/src/errors/database-connection-error.ts
@@ -0,0 +1,9 @@
+export class DatabaseConnectionError extends Error {
+  reason = "Error connection to database";
+
+  constructor() {
+    super();
+
+    Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+  }
+}
---
+++ auth/src/middlewares/error-handler.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof RequestValidationError) {
+    console.log("handling this error as a request validation error");
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    console.log("handling this error as a db connection error");
+  }
+
+  res.status(400).send({
+    message: err.message,
+  });
+};
