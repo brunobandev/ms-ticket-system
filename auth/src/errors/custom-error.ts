ce4a4751849440c65fd511bf7c8effe2a8a86118
Bruno Bandeira <brunoban.dev@gmail.com>
2023-02-03 19:59:45 +0000

    adding mongo db

--- auth/src/errors/custom-error.ts
+++ auth/src/errors/custom-error.ts
@@ -1,6 +1,6 @@
 export abstract class CustomError extends Error {
   abstract statusCode: number;
-  constructor(message: string) {
+  protected constructor(message: string) {
     super(message);
 
     Object.setPrototypeOf(this, CustomError.prototype);
---
+++ auth/src/models/user.ts
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+// An interface that describes the properties
+// that are required to create a new user
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes the properties
+// that a User Model has
+interface UserModel extends mongoose.Model<any> {
+  build(attrs: UserAttrs): any;
+}
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+});
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = mongoose.model<any, UserModel>("User", userSchema);
+
+export { User };
